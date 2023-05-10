53c4e031c28baa9ed919f5a9d27c381fad71e892
Lim JangHee <limjackie@naver.com>
2023-05-10 09:05:26 +0900

    ~0509 복구

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+// return x, parameter x
+function hi() {
+    // alert("hi!!!!!")
+    console.log("log!!!");
+}
+
+// return x, parameter o
+function sum(x, y) {
+    let result = x + y;
+    console.log(result)
+}
+
+// return o, parameter o
+function total(x, y) {
+    return x + y;
+}
